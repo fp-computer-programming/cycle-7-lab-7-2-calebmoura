7dee067efcb4fdc2f8a1be25013860504dea25b3
calebmoura <144838461+calebmoura@users.noreply.github.com>
2023-11-29 05:20:58 -0500

    Add files via upload

---
+++ Lab_7-2.py
@@ -0,0 +1,15 @@
+# Author: Caleb Moura
+
+def find_sum(num1, num2):
+    # Add the arguments and store the result in num_sum.
+    num_sum = num1 + num2
+    # Returning the sum
+    return num_sum
+
+# Call function to find the sum 111 and 222.
+my_sum = find_sum(111, 222)
+
+# Print the result of the two numbers.
+print("The sum is:", my_sum)
+
+# Terminal prints "The sum is 333" which is correct
